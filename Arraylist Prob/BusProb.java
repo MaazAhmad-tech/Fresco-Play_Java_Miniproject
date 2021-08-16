51e6133a73e3e40f6e62de824b503048a9d75ad6
Maaz <maaz.ahmad1912@gmail.com>
2021-08-16 16:11:35 +0530

    Some more question updates

--- Arraylist Prob/BusProb.java
+++ Arraylist Prob/BusProb.java
@@ -1,7 +1,9 @@
-package com.fresco;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*;
+import java.text.DecimalFormat;
 class Passanger
 {
     int id;
@@ -28,107 +30,129 @@ class Passanger
         this.fare=fare;
     }
 }
-public class BusProb {
+class BusProb {
 
     public String output(int capacity, int stops, List<String> listOfInputStrings, String query)
     {
-        String outstring="";
+
         //Write your code here
-        ArrayList<Passanger> passengerList = new ArrayList<Passanger>();
-        ArrayList<String> passengerIn = new ArrayList<String>();
-        ArrayList<String> passengerOut = new ArrayList<String>();
-        ArrayList<String> passengerPays06 = new ArrayList<String>();
-        ArrayList<String> passengerPays03 = new ArrayList<String>();
-        ArrayList<String> passengerPays = new ArrayList<String>();
-        int onTheBus=0;
-        int outTheBus=0;
+        ArrayList<String>   dummy     =     new ArrayList<>();
+        Map<String,Integer> hash = new HashMap<String,Integer>();
+        Map<String,Double> cust_fare = new HashMap<String,Double>();
+        Map<String,Integer> times=new HashMap<String,Integer>();
+        int a=0,b=0,c=0;
+        // Set<String> hash_Set= new HashSet<String>();
+        double[][] arr=new double[stops][6];
+        int k=0;
+        int cnt=0;
+        int totalin=0,totalout=0;
+        int in=0,out=0;
+        int demp=0;
+        for(String f:listOfInputStrings)
+        {
+            StringTokenizer st = new StringTokenizer(f," ");
+            in=0;
+            out=0;
+            while (st.hasMoreTokens()) {
+                String tok=st.nextToken();
+                String tok2=tok.substring(1,tok.length());
+                if(tok.charAt(0)=='+')
+                {
+                    in++;
 
-        // looping through number of stops
-        for (int i = 0; i < stops; i++) {
-            int currentPassCount = 0;
-            float fare = 0f;
-
-            // split listOfInputStrings to get all passengers
-            String[] currentPass = listOfInputStrings.get(i).split(" ");
-
-            currentPassCount = passengerIn.size();
-            // looping through each passenger at one stop to count number of passengers in bus
-            for (String p : currentPass) {
-                // storing sign to add/remove passenger
-                char sign = p.charAt(0);
-                if (sign == '+') {
-                    onTheBus++;
-                    currentPassCount+=1;
-                } else {
-                    outTheBus++;
-                    currentPassCount-=1;
+                    hash.put(tok2,hash.getOrDefault(tok2, 0)+1);
+                    times.put(tok2,times.getOrDefault(tok2, 0)+1);
                 }
-                // System.out.println("passenger id = "+p+",currentcount = "+currentPassCount);
+                else{
+                    out++;
+                    hash.put(tok2,hash.getOrDefault(tok2, 0)-1);
+                }
+                if(hash.get(tok2)<=0)
+                    hash.remove(tok2);
+
+            }
+            double val=0.0;
+            cnt+=in;
+            cnt-=out;
+
+            if(cnt<=Math.ceil(((double)capacity/4)))
+            {val=1.6*capacity;a+=in;}
+            else if(cnt>Math.ceil(((double)capacity/4))&&cnt<=Math.ceil(((double)capacity/2)))
+            {val=1.3*capacity;b+=in;}
+            else if(cnt>Math.ceil(((double)capacity/2)))
+            {val=capacity;c+=in;}
+
+
+            StringTokenizer st2 = new StringTokenizer(f," ");
+            while (st2.hasMoreTokens()) {
+                String tok=st2.nextToken();
+                String tok2=tok.substring(1,tok.length());
+                if(tok.charAt(0)=='+'){
+                    cust_fare.put(tok2,cust_fare.getOrDefault(tok2,0.0)+val);
+                    // System.out.print(cust_fare.get(tok2)+ " ");
+                }
+
+            }
+            totalin+=in;
+            totalout+=out;
+            k++;
+        }
+
+        System.out.println(demp+" "+a+" "+b+" "+c+"\n");
+
+        StringTokenizer st3 = new StringTokenizer(query,",");
+        while(st3.hasMoreTokens())
+        {
+            String q=st3.nextToken();
+            System.out.println(q);
+            if(q.compareTo("1")==0)
+            {
+                System.out.println(cust_fare.size()+" waw");
+                return totalin+" passengers got on the bus and "+totalout+" passengers got out of the bus";
+            }
+            else if(q.compareTo("2")==0)
+            {
+                DecimalFormat df=new DecimalFormat("###.#");
+                // return a+" passenger spent a fare of "+1.6*capacity+", "+b+" passengers spent a fare of "+1.3*capacity+" and "+c+" passengers spent a fare of "+capacity;
+                // return a+" passengers traveled with a fare of "+1.6*capacity+", "+b+" passengers traveled with a fare of "+1.3*capacity+" and "+c+" passengers traveled with a fare of "+capacity;
+                String xx=a+" passengers traveled with a fare of "+(Math.round(1.6*capacity * 100.0) / 100.0)+", "+b+" passengers traveled with a fare of "+(Math.round(1.3*capacity * 100.0) / 100.0)+" and "+c+" passengers traveled with a fare of "+(Math.round(capacity * 100.0) / 100.0);
+                System.out.println(xx.hashCode());
+                System.out.println(a+" passengers traveled with a fare of "+(Math.round(1.6*capacity * 10.0) / 10.0)+", "+b+" passengers traveled with a fare of "+(Math.round(1.3*capacity * 10.0) / 10.0)+" and "+c+" passengers traveled with a fare of "+(Math.round(capacity * 10.0) / 10.0));
+                return a+" passengers traveled with a fare of "+(Math.round(1.6*capacity * 10.0) / 10.0)+", "+b+" passengers traveled with a fare of "+(Math.round(1.3*capacity * 10.0) / 10.0)+" and "+c+" passengers traveled with a fare of "+(Math.round(capacity * 10.0) / 10.0);
+            }
+            else if(q.compareTo("3")==0)
+            {
+                String tex=st3.nextToken();
+                tex=tex.trim();
+                System.out.println(cust_fare.get(tex)+" waw");
+                return "Passenger "+tex+" spent a total fare of "+(Math.round(cust_fare.get(tex) * 10.0) / 10.0);
+            }
+            else if(q.compareTo("4")==0)
+            {
+                String tex=st3.nextToken();
+                tex=tex.trim();
+                System.out.println(times.get(tex)+" waw"+" ");
+                return "Passenger "+tex+" has got on the bus for "+times.get(tex)+" times";
+
+            }
+            else
+            {
+                String tex=st3.nextToken();
+                tex=tex.trim();
+                String available;
+                if(hash.containsKey(tex))
+                    available="inside";
+                else
+                    available="not inside";
+                return "Passenger "+tex+" was "+available+" the bus at the end of the trip";
             }
 
-            // calculating fare till current stop
-            if (currentPassCount <= Math.ceil((double) capacity / 4)) {
-                fare = (float) (capacity + capacity * 0.6);
-            } else if (currentPassCount <= Math.ceil((double) capacity / 2)) {
-                fare = (float) (capacity + capacity * 0.3);
-            } else {
-                fare = (float) capacity;
-            }
-
-            // looping through each passenger at one stop
-            for (String p : currentPass) {
-                // storing sign to add/remove passenger
-                char sign = p.charAt(0);
-                // storing passengerId in string
-                String passnegerIdStr = p.substring(1);
-                int passnegerId = Integer.parseInt(passnegerIdStr);
-
-                // checking and adding passenger to passengerList
-                if (sign == '+') {
-                    boolean flag = true;
-                    for (Passanger psg : passengerList) {
-                        if (psg.getId() == passnegerId) {
-                            flag = false;
-                            float temp = psg.getFare();
-                            // update fare
-                            psg.setFare(fare + temp);
-                        }
-                    }
-                    if (flag) {
-                        passengerList.add(new Passanger(passnegerId, fare));
-                    }
-                    passengerOut.remove(passnegerIdStr);
-                    passengerIn.add(passnegerIdStr);
-                }
-                // checking and adding passenger to passengerList ends
-
-                else {
-                    passengerIn.remove(passnegerIdStr);
-
-                }
-            }
-
-            // }
-            // System.out.println("currentcount = "+currentPassCount);
-            // System.out.println("fare = "+fare);
         }
-        // System.out.println("on the bus = "+onTheBus+"out the bus = "+outTheBus);
-        if (query.equals("1")) {
-            outstring = onTheBus + " passengers got on the bus and "+outTheBus+" passengers got out of the bus";
 
-        }
-        else if (query.equals("2")) {
-            float f1 =
-        }
-        //  else if (query.equals("3")) {
 
-        // } else if (query.equals("4")) {
 
-        // } else if (query.equals("5")) {
 
-        // }
-
-        //passengerList.forEach((n) -> System.out.println(n.getFare()));
-        return outstring;
+        return "";
     }
+
 }
